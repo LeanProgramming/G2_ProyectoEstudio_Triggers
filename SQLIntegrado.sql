e88c7fc8bd42ac106a489c9c189ca8a7f1de9466
Joselo <joselo34@hotmail.com>
2023-11-18 19:17:59 -0300

    Agrega almacenado para cambiar importe de gasto

--- SQLIntegrado.sql
+++ SQLIntegrado.sql
@@ -173,3 +173,34 @@ BEGIN
     ROLLBACK;
 END
 GO
+
+USE [base_consorcio]
+GO
+
+DROP PROCEDURE IF EXISTS [CambiarImporteGasto]
+GO
+
+-- Procedimiento para cambiar importe de gastos
+
+CREATE PROCEDURE [CambiarImporteGasto] (
+	@idgasto int = null,
+	@importe decimal(8,2) = null,
+	@exito bit OUT,
+	@error varchar(200) OUT
+)
+AS 
+BEGIN
+SET @exito = 1
+BEGIN TRY
+	BEGIN TRAN
+	UPDATE gasto
+	SET importe=@importe
+	WHERE idgasto=@idgasto
+	COMMIT TRAN
+END TRY
+BEGIN CATCH
+	SET @error = ERROR_MESSAGE()
+	ROLLBACK TRAN;
+	SET @exito = 0
+END CATCH
+END
