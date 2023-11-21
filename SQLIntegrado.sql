1e43afb6c90d44d516d67a5e47912842cd0c92a6
Lean M <munozleandrom96@gmail.com>
2023-11-21 19:17:10 -0300

    Agregado scrip de creación para el procedimiento de cambiar residencia del administrador.

--- SQLIntegrado.sql
+++ SQLIntegrado.sql
@@ -46,6 +46,37 @@ BEGIN CATCH
 END CATCH
 END
 
+
+--Creación de Procedimiento para cambiar residencia del administrador
+DROP PROCEDURE IF EXISTS [CambiarResidenciaAdministrador]
+GO
+
+CREATE PROCEDURE [CambiarResidenciaAdministrador] (
+	@idadmin int = null,
+	@viveahi varchar(1) = null,
+	@exito bit OUT,
+	@error varchar(200) OUT
+)
+AS 
+BEGIN
+SET @exito = 1
+BEGIN TRY
+	BEGIN TRAN
+	UPDATE administrador
+	SET viveahi=@viveahi
+	WHERE idadmin=@idadmin
+	COMMIT TRAN
+END TRY
+BEGIN CATCH
+	SET @error = CASE
+					WHEN ERROR_MESSAGE() LIKE '%CK_habitante_viveahi%' THEN 'Valor incorrecto para el campo viveahi. Los valores posibles son S y N.'
+					ELSE ERROR_MESSAGE()
+					END
+	ROLLBACK TRAN;
+	SET @exito = 0
+END CATCH
+END
+
 /*
 Creación de tablas auxiliares de auditoría y creación de TRIGGERS
 */
